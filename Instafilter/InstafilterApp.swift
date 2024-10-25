5e3049ee1e73ad806b658ec014aceccc8dc14074
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-25 12:29:34 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Juan Carlos Robledo Morales on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstafilterTests/InstafilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterTests.swift
+//  InstafilterTests
+//
+//  Created by Juan Carlos Robledo Morales on 25/10/24.
+//
+
+import Testing
+@testable import Instafilter
+
+struct InstafilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
