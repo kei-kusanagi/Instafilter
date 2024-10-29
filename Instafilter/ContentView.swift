ea310f6b320c90152198907dc6a58f148527454d
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-29 12:34:32 -0600

    Loading photos from the user's photo library

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,63 +6,63 @@
 //
 
 import SwiftUI
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
+import SwiftUI
+
 
 struct ContentView: View {
-    @State private var image: Image?
-    var body: some View{
-        ContentUnavailableView {
-            Label("No snippets", systemImage: "swift")
-        } description: {
-            Text("You don't have any saved snippets yet.")
-        } actions: {
-            Button("Create Snippet") {
-                // create a snippet
-            }
-            .buttonStyle(.borderedProminent)
-        }
-    }
+//    @State private var pickerItem: PhotosPickerItem?
+    @State private var pickerItems = [PhotosPickerItem]()
 
-//    var body: some View {
-//        VStack {
-//            image?
-//                .resizable()
-//                .scaledToFit()
+    @State private var selectedImage: Image?
+    
+    @State private var selectedImages = [Image]()
+
+
+    var body: some View{
+        VStack {
+//            PhotosPicker("Selecciona imágenes", selection: $pickerItems, matching: .images)
+//            PhotosPicker("Selecciona imágenes", selection: $pickerItems, maxSelectionCount: 3, matching: .images)
+//            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .images) {
+//                Label("Selecciona una foto", systemImage: "photo")
+//            }
+            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
+                Label("Selecciona una foto", systemImage: "photo")
+            }
+
+
+//            PhotosPicker("Selecciona una foto", selection: $pickerItem, matching: .images)
+            selectedImage?
+                .resizable()
+                .scaledToFit()
+
+        }
+//        .onChange(of: pickerItem) {
+//            Task {
+//                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+//            }
 //        }
-//        .onAppear(perform: loadImage)
-//    }
-//
-//    func loadImage() {
-//        let inputImage = UIImage(resource: .example)
-//        let beginImage = CIImage(image: inputImage)
-//        
-//        let context = CIContext()
-//        let currentFilter = CIFilter.pixellate()
-//
-//        currentFilter.inputImage = beginImage
-//        
-//        let amount = 1.0
-//
-//        let inputKeys = currentFilter.inputKeys
-//
-//        if inputKeys.contains(kCIInputIntensityKey) {
-//            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-//        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
-//        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
-//        
-//        // get a CIImage from our filter or exit if that fails
-//        guard let outputImage = currentFilter.outputImage else { return }
-//
-//        // attempt to get a CGImage from our CIImage
-//        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-//
-//        // convert that to a UIImage
-//        let uiImage = UIImage(cgImage: cgImage)
-//
-//        // and convert that to a SwiftUI image
-//        image = Image(uiImage: uiImage)
-//    }
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
+
+        ScrollView {
+            ForEach(0..<selectedImages.count, id: \.self) { i in
+                selectedImages[i]
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+
+    }
 }
 
 
